9aa5a266cf30590825e308831508b63f42413732
wafflejuice <wafflejuice96@gmail.com>
2022-09-30 23:22:49 +0900

    Assignment - 당근마켓 레이아웃 따라하기

--- lib/main.dart
+++ lib/main.dart
@@ -11,24 +11,45 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     var app = MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('This is an app'),
-        ),
-        body: Text('Hi'),
-        bottomNavigationBar: BottomAppBar(
-          child: SizedBox(
-            height: 70,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Icon(Icons.phone),
-                Icon(Icons.message),
-                Icon(Icons.contact_page),
-              ],
-            ),
+          appBar: AppBar(
+            actions: [
+              Icon(Icons.star),
+              Icon(Icons.star),
+            ],
+            leading: Icon(Icons.star),
+            title: Text('instagram'),
           ),
-        ),
-      ),
+          body: Column(
+            children: [
+              Container(
+                padding: EdgeInsets.all(10),
+                child: Row(
+                  children: [
+                    Image(image: AssetImage('assets/doge.png'), width: 120),
+                    SizedBox(width: 6),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('캐논 DSLR 100D (단렌즈, 충전기 16기가SD 포함)',
+                              style: TextStyle(fontSize: 16)),
+                          Text('성동구 행당동·끌올 10분 전',
+                              style: TextStyle(fontSize: 12)),
+                          Text('210,000원',
+                              style: TextStyle(
+                                  fontSize: 12, fontWeight: FontWeight.bold)),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [Icon(Icons.heart_broken), Text('4')],
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          )),
     );
 
     return app;
