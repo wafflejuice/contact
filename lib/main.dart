ea00fe058936d6d8b61df8e3889c87657f7aea03
wafflejuice <wafflejuice96@gmail.com>
2022-10-01 13:32:15 +0900

    Assignment - list view & custom widget

--- lib/main.dart
+++ lib/main.dart
@@ -11,47 +11,57 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     var app = MaterialApp(
       home: Scaffold(
-          appBar: AppBar(
-            actions: [
-              Icon(Icons.star),
-              Icon(Icons.star),
-            ],
-            leading: Icon(Icons.star),
-            title: Text('instagram'),
-          ),
-          body: Column(
-            children: [
-              Container(
-                padding: EdgeInsets.all(10),
-                child: Row(
-                  children: [
-                    Image(image: AssetImage('assets/doge.png'), width: 120),
-                    SizedBox(width: 6),
-                    Expanded(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text('캐논 DSLR 100D (단렌즈, 충전기 16기가SD 포함)',
-                              style: TextStyle(fontSize: 16)),
-                          Text('성동구 행당동·끌올 10분 전',
-                              style: TextStyle(fontSize: 12)),
-                          Text('210,000원',
-                              style: TextStyle(
-                                  fontSize: 12, fontWeight: FontWeight.bold)),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.end,
-                            children: [Icon(Icons.favorite), Text('4')],
-                          )
-                        ],
-                      ),
-                    )
-                  ],
-                ),
+        appBar: AppBar(),
+        body: ListView(
+          children: [
+            Row(children: [
+              Icon(Icons.account_circle),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text('John'),
               ),
-            ],
-          )),
+            ]),
+            Row(children: [
+              Icon(Icons.account_circle),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text('John'),
+              ),
+            ]),
+            Row(children: [
+              Icon(Icons.account_circle),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text('John'),
+              ),
+            ]),
+          ],
+        ),
+        bottomNavigationBar: BottomBar(),
+      ),
     );
 
     return app;
+  }
+}
+
+class BottomBar extends StatelessWidget {
+  const BottomBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomAppBar(
+      child: SizedBox(
+        height: 60,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Icon(Icons.phone),
+            Icon(Icons.message),
+            Icon(Icons.contact_page),
+          ],
+        ),
+      ),
+    );
   }
 }
